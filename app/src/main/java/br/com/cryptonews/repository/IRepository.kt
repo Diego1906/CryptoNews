c68261dc88c77328f23012fb993a35865212a797
Diego <di_fsantos@yahoo.com.br>
2020-03-10 12:23:42 -0300

    Implementação da camada repository

---
+++ app/src/main/java/br/com/cryptonews/repository/IRepository.kt
@@ -0,0 +1,13 @@
+package br.com.cryptonews.repository
+
+import br.com.cryptonews.entities.News
+
+interface IRepository {
+
+    suspend fun getRemoteListCryptoNews(
+        qInTitle: String,
+        dateFrom: String,
+        dateTo: String,
+        apiKey: String
+    ): News
+}
---
+++ app/src/main/java/br/com/cryptonews/repository/Repository.kt
@@ -0,0 +1,16 @@
+package br.com.cryptonews.repository
+
+import br.com.cryptonews.entities.News
+import br.com.cryptonews.remote.IService
+
+class Repository(val service: IService) : IRepository {
+
+    override suspend fun getRemoteListCryptoNews(
+        qInTitle: String,
+        dateFrom: String,
+        dateTo: String,
+        apiKey: String
+    ): News {
+        return service.getService().getRemoteListCryptoNews(qInTitle, dateFrom, dateTo, apiKey)
+    }
+}
