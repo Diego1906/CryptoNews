2c043f45ffebd54406dd31c72711d0cb082b9822
Diego <di_fsantos@yahoo.com.br>
2020-03-10 09:47:11 -0300

    Criação do Recyclerview no fragment news e menu de items

---
+++ app/src/main/java/br/com/cryptonews/ui/NewsFragment.kt
@@ -0,0 +1,22 @@
+package br.com.cryptonews.ui
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import br.com.cryptonews.R
+
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_items, menu)
+    }
+}
